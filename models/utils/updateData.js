827355087deab65a716f6235ad616226451864e3
phtf92 <phtf92@gmail.com>
2020-07-02 16:40:14 -0300

    CC

--- models/utils/updateData.js
+++ models/utils/updateData.js
@@ -2,14 +2,13 @@ const connection = require('../connection');
 
 const updateData = async (collection, itemToUpdate, { name, quantity }) => {
   const db = await connection();
-  await db.collection(collection)
-    .updateOne(itemToUpdate,
-      {
-        $set: {
-          name,
-          quantity,
-        },
-      });
+  await db.collection(collection).updateOne(itemToUpdate,
+    {
+      $set: {
+        name,
+        quantity,
+      },
+    });
 
   return db.collection(collection).find(itemToUpdate).toArray();
 };
