4b921ee0851a7e382f9b47c997d2c1816171b096
Anderson Bolivar <bolivar.anderson@hotmail.com>
2020-07-10 01:33:17 -0300

    Code Climate - 01

--- middlewares/errorMiddleware.js
+++ middlewares/errorMiddleware.js
@@ -18,7 +18,7 @@ function errorBoom(err, res) {
   return response.json({ error: { message: data, code: message } });
 }
 
-function errorMiddleware(err, req, res, next) {
+function errorMiddleware(err, _req, res, _next) {
   if (err.isBoom) {
     return errorBoom(err, res);
   }
@@ -27,9 +27,3 @@ function errorMiddleware(err, req, res, next) {
 }
 
 module.exports = errorMiddleware;
-
-// - Caso o recurso não seja encontrado, sua API retorne o status HTTP adequado com o body `{ message: '<recurso> não encontrado' }`
-//   - Em caso de erro, sua API retorne o status HTTP adequado com o body `{ error: { message: <mensagem de erro>, code: <código do erro> } }`
-//     - O código do erro deve ser determinado por você e deve seguir o mesmo padrão para toda a aplicação. Por exemplo: `'not_found'`, `'invalid_data'` e afins
-//   - Em caso de dados inválidos, sua API retorne o status HTTP adequado, com o body `{ error: { message: 'Dados inválidos', code: <código do erro> } }`.
-//   - Todos os retornos de erro devem seguir o mesmo formato. Para erros que requerem dados adicionais (por exemplo, para informar quais campos estão incorretos) utilize a propriedade `data` dentro do objeto `error`.
--- services/joinSchemas.js
+++ services/joinSchemas.js
@@ -1,10 +1,19 @@
 const Joi = require('@hapi/joi');
 
 const productsSchema = Joi.object({
-  name: Joi.string().min(5).required(),
-  quantity: Joi.number().prefs({ convert: false }).integer().min(0).required(),
-})
-.unknown(false);
+  name:
+    Joi
+    .string()
+    .min(5)
+    .required(),
+  quantity:
+    Joi
+      .number()
+      .prefs({ convert: false })
+      .integer()
+      .min(0)
+      .required(),
+}).unknown(false);
 
 module.exports = {
   productsSchema,
