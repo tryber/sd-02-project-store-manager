85aab80ebac7aaf13f3c3c234c20e62cd1837e0f
eddyeboy27 <lipe0503@gmail.com>
2020-07-15 04:51:43 -0300

    CC Issues FIX EndPointError

--- controllers/endPointError.js
+++ controllers/endPointError.js
@@ -1,6 +1,5 @@
-const endPointError = async (req, res, next) => {
-  return res.status(404).json({ error: { message: 'Endpoint not found', code: 404 } });
-};
+const endPointError = async (req, res) =>
+  res.status(404).json({ error: { message: 'Endpoint not found', code: 404 } });
 
 module.exports = {
   endPointError,
